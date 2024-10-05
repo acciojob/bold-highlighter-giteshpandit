013efa23828b6bddba623b45ed9849a518f31f43
giteshpandit <panditgitesh38@gmail.com>
2024-10-05 14:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// This function will be called when you hover over the link
 function highlight() {
-    //Write your code here
-
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
+    // Get all <strong> elements in the document
+    const boldWords = document.querySelectorAll('strong');
     
+    // Loop through each <strong> element and change the color to green
+    boldWords.forEach(function(word) {
+        word.style.color = 'green';
+    });
 }
+
+// This function will be called when you hover out of the link
+function return_normal() {
+    // Get all <strong> elements in the document
+    const boldWords = document.querySelectorAll('strong');
+    
+    // Loop through each <strong> element and change the color back to black
+    boldWords.forEach(function(word) {
+        word.style.color = 'black';
+    });
+}
+
